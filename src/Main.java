b472ae7455a1525fefcfa84d0a586b6b3d2be3a2
serkan-cimen <126679909+serkan-cimen@users.noreply.github.com>
2023-05-30 09:47:58 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl giriniz : ");
+        int yil = input.nextInt();
+
+        boolean artikYil = false;
+
+        if (yil % 4 == 0) {
+            if (yil % 100 == 0) {
+                if (yil % 400 == 0){
+                    artikYil = true;
+                }
+            } else {
+                artikYil = true;
+            }
+        }
+
+        if (artikYil) {
+            System.out.println(yil + " bir artık yıldır.");
+        } else {
+            System.out.println(yil + " bir artık yıl değildir.");
+        }
+    }
+}
